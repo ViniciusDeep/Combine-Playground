ff0b70afa1e7ca20f5de5cfee52bbe1694292f0a
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-08-04 22:29:50 -0300

    Config playground

---
+++ Combine.playground/Pages/Framework.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+/*:
+ # Framework
+  Varios conceitos do framework são bem interessantes o primeiro que vamos ver são sobre os Publishers e Subscribers
+*/
+
+
+
+
+
+
+//: [Next](@next)
---
+++ Combine.playground/Pages/Reatividade.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+/*:
+ # Combine
+*/
+/*:
+  Combine é um framework que trabalha com reatividade de forma nativa no iOS.
+*/
+/*:
+ ## Reatividade
+    Programação reativa é um paradgma de programação orientado ao fluxo de dados assíncrono, que está ligado diretamente ao registro e acontecimento de eventos.
+*/
+/*:
+ ![Exemplo ](combine.png)
+*/
+/*:
+ Existem várias formas e conceitos necessários quando se estuda programação reativa, um deles é um padrão de projeto chamado Observer que consiste em um cojunto de componentes que são necessários para montar uma seguinte logistica
+ */
+/*:
+ ![Padrão: Observer ](observer.png)
+*/
+/*:
+  Traduzindo temos um Subject que será um sujeito a ser seguido no twitter, quem irá segui-lo serão os Observers quando o evento de follow for enviado para o subject, os observer passarão a escutar as propagações de mudanças derivadas do subject, quando algum dado for de certa forma alterado essas mudanças são propagadas e um observer pode manipu-las,esses dados, eventos e mudanças propagadas são chamados de Observable
+*/
+/*:
+  Já que sabemos um pouco mais da parte conceitual agora vamos lá para os conceitos do próprio framework
+*/
+//: [Next](@next)
+
