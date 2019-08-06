07f1966f6b69cdf9847fb659ebe4b1dde0b7364d
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-08-06 08:18:39 -0300

    Add pages of operator and Framework

--- Combine.playground/Pages/Framework.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Framework.xcplaygroundpage/Contents.swift
@@ -1,12 +1,100 @@
 //: [Previous](@previous)
 /*:
  # Framework
-  Varios conceitos do framework são bem interessantes o primeiro que vamos ver são sobre os Publishers e Subscribers
+  Varios conceitos do framework são bem interessantes, o primeiro que vamos ver são sobre os Publishers e Subscribers
+*/
+/*:
+ ## Publisher
+ 
+ É um protocolo no qual quem assina pode entrar uma sequência de valores ao longo tempo,  quando você implementa esse protocolo, ele pede que você diga qual o tipo de saída você deseja que alguém retorne, e a possibilidade de erro.
+ 
+ */
+import Combine
+import UIKit
+
+class MyPublisher: Publisher {
+    typealias Output = String
+    typealias Failure = Error //Você pode também pode declarar Never se não existir nenhum erro
+
+    // Essa função é chamada para anexar um Subscriber para esse Publisher
+    func receive<S>(subscriber: S) where S : Subscriber, MyPublisher.Failure == S.Failure, MyPublisher.Output == S.Input {
+           print("Register Subscriber")
+       }
+}
+/*:
+ ## Subscribers
+ Um subscriber irá agir sobre um evento, os publishers somente emitiram valores quando forem explicitamente solicitados pelo seu subscriber
 */
 
+var publisher = Just(19)
 
+publisher.map { (number)  in
+    return "Vinicius tem \(number) anos"
+}.sink { (value) in
+    print(value)
+}
+/*:
+ # Personalizando nosso próprio Publisher
+ */
+/*:
+ Para começar criaremos nossa própria Subscription
+ */
 
+final class UIControlSubscription<SubscriberType: Subscriber, Control: UIControl>: Subscription where SubscriberType.Input == Control {
+    
+    var subscriber: SubscriberType?
+    
+    private let control: Control
+    
+    init(subscriber: SubscriberType, control: Control, event: UIControl.Event){
+        self.subscriber = subscriber
+        self.control = control
+        control.addTarget(self, action: #selector(eventHandler), for: event)
+    }
+    
+    func request(_ demand: Subscribers.Demand) {}
+    
+    func cancel() {
+        subscriber = nil
+    }
+    
+    @objc func eventHandler() {
+        _ = subscriber?.receive(control)
+    }
+    
+}
 
+class UIControlPublisher<Control: UIControl>: Publisher {
+    typealias Output = Control
+    
+    typealias Failure = Never
+    
+    let control: Control
+    let controlEvents: UIControl.Event
+    
+    init(control: Control, events: UIControl.Event) {
+        self.control = control
+        self.controlEvents = events
+    }
+    
+    func receive<S>(subscriber: S) where S : Subscriber, UIControlPublisher.Failure == S.Failure, UIControlPublisher.Output == S.Input {
+        let subscription = UIControlSubscription(subscriber: subscriber, control: control, event: controlEvents)
+        subscriber.receive(subscription: subscription)
+    }
+}
 
+/*:
+ ## Publisher + Subscriber
+ 
+ ### Publisher
+ * Define com valores e erros são produzidos
+ * Value Type
+ * Permite os registros pelos subscribers
+ 
+ ### Subscriber
+  * Recebe os valores
+  * Reference Type
+  * Recebe os valores e pode reagir com uma ação
+ */
 
 //: [Next](@next)
---
+++ Combine.playground/Pages/Operadores.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+//: [Previous](@previous)
+
+/*:
+ # Operadores
+ Apesar de programação reativa não ser sinônimo de reatividade, no Combine temos diversos operadores funcionais alguns muito semelhantes com os operadores nativos da própria linguagem.
+ */
+import Combine
+import UIKit
+/*:
+ ## Map
+ O Map do Combine é bem parecido com o Swift Map, ele serve para transforma uma sequência de valores no tipo desejado, que no nosso caso utilizaremos int
+ */
+_ = Publishers.Sequence(sequence: [1,2,4]).map { $0 * 10}.flatMap{Just($0)}.sink(receiveValue: {
+    print($0)
+})
+/*:
+ ## Filter
+ O Filter serve para filtrar os valores em sequência por exemplo se um número é par, pegando o elemento e sabendo se o módulo da divisão é 0, nesse caso queremos saber os elementos da sequência
+ */
+_ = Publishers.Sequence(sequence: [1,2,2,3,3,4,7])
+    .map { $0 * 2 }
+    .flatMap { Just($0) }
+    .filter { $0.isMultiple(of: 2) }
+    .dropFirst(3)
+    .removeDuplicates()
+    .sink(receiveValue: { value in
+        print(value)
+    })
+/*:
+ ## Merge
+ O merge serve para você unir tipo de coleções do mesmo tipo em um só, no exemplo abaixo mergaremos de tipo String.
+ */
+
+let cearaCities = PassthroughSubject<String, Never>()
+let saoPauloCities = PassthroughSubject<String, Never>()
+let franceCities = PassthroughSubject<String, Never>()
+
+let brazilCities = Publishers.Merge(saoPauloCities, cearaCities)
+
+_ = brazilCities.sink(receiveValue: { city in
+    print("\(city) is a city from Ceará")
+})
+
+cearaCities.send("Maracanaú")
+franceCities.send("Paris")
+saoPauloCities.send("Campinas")
+
+
+/*:
+ ## Schedulers
+ Um dos princípais problemas da programação reativa é o back-pressure, como você tá trabalhando com dados assíncronos, muitas vezes pode acontecer que os dados veem de uma forma gigansteca e você não tenha controle sobre eles, para resolver isso pode escalar um tempo no qual o evento venha  ser executado, como o exemplo abaixo mostra
+ */
+
+
+let selectedFilter = PassthroughSubject<String, Never>()
+
+let searchTextField: UITextField = {
+    let tf = UITextField()
+    tf.text = "Valor digitado..."
+    return tf
+}()
+
+let searchText = NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: searchTextField).map({ ($0.object as! UITextField).text  })
+    .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+    .eraseToAnyPublisher()
+
+
+//: [Next](@next)
