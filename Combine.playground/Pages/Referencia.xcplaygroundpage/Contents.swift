1190296d730286137a665e53e6d5e7505c45b0dc
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-08-15 23:04:31 -0300

    Updatge resources

--- Combine.playground/Pages/Referencia.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Referencia.xcplaygroundpage/Contents.swift
@@ -3,7 +3,25 @@
 /*:
  Espero que tenham aprendido um pouco mais sobre o framework, aqui serão listadas todas as fontes de onde pesquisei todas as informações
  
+ Combine VS RX Swift
  
+ Publishers are Observables
+ Subscribers are Observers
+ Cancellable is Disposable
+ CurrentValueSubject is BehaviorSubject
+ PassthroughSubject is PublishSubject
+ handleEvents() is do()
  */
 
+    
+//: [Combine](https://medium.com/@miraskarazhigitov/swift-combine-introduction-82647a9d8bfc)
+
+//: [Newtwork request with Combine](https://medium.com/@09mejohn/network-request-with-swift-combine-4ae9e5cc751f)
+
+//: [Swift MVVM + Combine](https://engineering.yoyowallet.com/swift-mvvm-and-combine-2c4825227c60)
+
+
+
+
 //: [Next](@next)
+
